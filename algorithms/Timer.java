ea756159b14050177c92776df57a3d31ddd9927a
CFlory-Programming <colestoneflory@gmail.com>
2026-02-09 11:34:16 -0700

    Added timer

---
+++ algorithms/Timer.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+public class Timer {
+
+    public static long timeSearch(Runnable search) {
+        long start = System.nanoTime();
+        search.run();
+        return System.nanoTime() - start;
+    }
+
+    public static long timeSort(Runnable sort) {
+        long start = System.nanoTime();
+        sort.run();
+        return System.nanoTime() - start;
+    }
+}
