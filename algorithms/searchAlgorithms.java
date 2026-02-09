e3f734da709f6e23d869c098efb728c70ecd5e2c
Emil-Gruenwald <gruenwald.emil@gmail.com>
2026-02-09 11:46:35 -0700

    Started adding algorithms

--- algorithms/searchAlgorithms.java
+++ algorithms/searchAlgorithms.java
@@ -4,5 +4,31 @@ import java.util.List;
 import model.Product;
 
 public class searchAlgorithms {
-    
+    public static Product linearSearch(List<Product> products, String sku) {
+        for (Product product : products) {
+            if (product.getSKU().equals(sku)) {
+                return product;
+            }
+        }
+        return null; // Not found
+    }
+
+    public static Product binarySearch(List<Product> products, String sku) {
+        int left = 0;
+        int right = products.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            Product midProduct = products.get(mid);
+
+            if (midProduct.getSKU().equals(sku)) {
+                return midProduct; // Found
+            } else if (midProduct.getSKU().compareTo(sku) < 0) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        return null; // Not found
+    }
 }
--- algorithms/sortAlgorithms.java
+++ algorithms/sortAlgorithms.java
@@ -4,5 +4,38 @@ import java.util.List;
 import model.Product;
 
 public class sortAlgorithms {
-    
+    public static void insertionSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void selectionSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void bubbleSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void mergeSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void quickSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void heapSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
+
+    public static void binaryInsertionSort(List<Product> products, boolean ascending, boolean sortByPrice) {
+
+
+    }
 }
