edd000d751b291655d6907d94de7dd232ee56abc
CFlory-Programming <colestoneflory@gmail.com>
2026-02-10 20:11:01 -0700

    Updated search algorithm

--- algorithms/searchAlgorithms.java
+++ algorithms/searchAlgorithms.java
@@ -18,12 +18,12 @@ public class SearchAlgorithms {
         int right = products.size() - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; // Get the middle number not the middle of the distance to get to the left and right numbers
             Product midProduct = products.get(mid);
 
             if (midProduct.getSKU().equals(sku)) {
                 return midProduct; // Found
-            } else if (midProduct.getSKU().compareTo(sku) < 0) {
+            } else if (midProduct.getSKU().compareTo(sku) < 0) { // mid SKU is less than target SKU
                 left = mid + 1; // Search in the right half
             } else {
                 right = mid - 1; // Search in the left half
